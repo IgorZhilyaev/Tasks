f25a4ab94789fca9af0d1527f84e790e85a97218
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-05 18:38:50 +0300

    Программа вычисления квадрата числа

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести квадрат числа
+Console.Write("Введите число-> "); // выводим на консоль сообщение
+string? namberStr = Console.ReadLine(); // вводим строковую переменную 
+int namber = int.Parse(namberStr); // преобразуем строковую переменную в int
+Console.WriteLine($"Квадрат числа: {namber * namber}"); //Console.WriteLine("Квадрат числа: " + namber * namber);
