5cfc2d5deee178c28cb42a25252187bb75411f50
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-06 23:35:17 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
 
-Console.WriteLine("Проверка кратности двух чисел");
+/*Console.WriteLine("Проверка кратности двух чисел");
 Console.Write("Введите первое число: ");
 string? aStr = Console.ReadLine();
 int a = int.Parse(aStr);
@@ -23,4 +23,37 @@ if (p == 0)
 else
 {
     Console.WriteLine("Второе число не кратно первому, остаток равен: " + p);
+}*/
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(0, 100);
+    return randomNumber;
 }
+
+int number = GetNumber();
+Console.WriteLine($"Первое случйное число {number}");
+
+int secondNumber = GetNumber();
+Console.WriteLine($"Второе случйное число {secondNumber}");
+
+void IsMultiple(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        int result = firstNumber % secondNumber;
+        if (result == 0)
+            Console.WriteLine("Первое число кратно второму");
+        else
+            Console.WriteLine($"Числа не кратны, остаток равен {result}");
+    }
+    else
+    {
+        int result = secondNumber % firstNumber;
+        if (result == 0)
+            Console.WriteLine("Второе число кратно первому");
+        else
+            Console.WriteLine($"Числа не кратны, остаток равен {result}");
+    }
+}
+IsMultiple(number, secondNumber);
