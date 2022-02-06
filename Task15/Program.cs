0efcb5638878352b8f9405f2e49d138bf3b123ba
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-06 23:51:01 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int number = 161;
+
+bool CheckOfModed(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+if (CheckOfModed(number))
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+else
+    Console.WriteLine($"Число {number}  не кратно 7 и 23");
