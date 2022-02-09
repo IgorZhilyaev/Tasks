b2a8de74fa7944bb118075fd89f2ffc616bb7beb
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-09 20:21:22 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ Task012/Program.cs
@@ -0,0 +1,19 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(100, 999);
+    return randomNumber;
+}
+
+int number = GetNumber();
+
+Console.Write($"Случайое трёхзначное число: {number}");
+
+string numberStr = number.ToString();
+
+string numberRemove = numberStr.Remove(numberStr.Length - 2, 1);
+
+Console.WriteLine();
+
+Console.Write($"Число после удаления второй цифры: {numberRemove}");
