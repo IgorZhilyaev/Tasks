1d5dbea9a2b30bef31791dfd31ac9f08433c8845
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-09 19:10:37 +0300

    Показать вторую цифру трёхзначного числа

---
+++ Task010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(100, 999);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine($"Случайное трёхзначное число: {number}");
+
+string numberStr = number.ToString();
+Console.WriteLine($"Вторая цифра этого числа: {numberStr[1]}");
+
+
