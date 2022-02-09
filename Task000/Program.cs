1faca73682dddf5034e4c172d3e469ebdfb319ef
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-09 19:27:24 +0300

    Вывести квадрат числа

---
+++ Task000/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вывести квадрат числа
+Console.Write("Введите число-> "); // выводим на консоль сообщение
+string? namberStr = Console.ReadLine(); // вводим строковую переменную 
+int namber = int.Parse(namberStr); // преобразуем строковую переменную в int
+Console.WriteLine($"Квадрат числа: {namber * namber}"); //Console.WriteLine("Квадрат числа: " + namber * namber);
