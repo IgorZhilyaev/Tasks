9596c398c0f21bbcc5d10af601b09bd189eccc46
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-13 14:38:47 +0300

    По двум заданным числам проверять является ли одно квадратом другого

---
+++ Task017/Program.cs
@@ -0,0 +1,25 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+int InPut(string mess)
+{
+    Console.Write(mess);
+    string? num = Console.ReadLine();
+    int a = int.Parse(num);
+    return a;
+}
+
+int numOne = InPut("Введите первое число: ");
+int numTwo = InPut("Введите второе число: ");
+
+if (numOne == numTwo * numTwo)
+{
+    Console.WriteLine($"{numOne} является квадратом {numTwo}");
+}
+else if (numTwo == numOne * numOne)
+{
+    Console.WriteLine($"{numTwo} является квадратом {numOne}");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратами друг друга");
+}
