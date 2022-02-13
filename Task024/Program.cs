33d4e34a0e3ef52d3b236606e7505ccdbb8a2a11
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-13 18:10:55 +0300

    Найти кубы чисел от 1 до N.

---
+++ Task024/Program.cs
@@ -0,0 +1,33 @@
+﻿// Найти кубы чисел от 1 до N.
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string? num = Console.ReadLine();
+    return int.Parse(num);
+}
+
+int[] InitArray(int number)
+{
+    int[] array = new int[number];
+
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+int n = InPut("Введите число больше 1: ");
+
+var array = InitArray(n);
+
+PrintArray(array);
