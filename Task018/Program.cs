1639519ce4b32e8ffb360b70cc8222dc5f63ea62
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-14 12:04:07 +0300

    Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y.

---
+++ Task018/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y.
+
+void Bool(bool x, bool y)
+{
+    bool a = !(x || y) == !x && !y;
+    Console.WriteLine($"При X = {x}, Y = {y} утверждение принимает значание: {a}");
+}
+
+Bool(false, false);
+Bool(false, true);
+Bool(true, false);
+Bool(true, true);
