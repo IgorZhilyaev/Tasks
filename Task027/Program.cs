cb00219889f0afa1e9d174e227bf5ab941a201bb
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-15 15:12:16 +0300

    Определить количество цифр в числе.

---
+++ Task027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить количество цифр в числе.
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(100, 1000000);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine(number);
+
+int countOfDigit = 0;
+
+while(number != 0)
+{
+    number = number/10;
+    countOfDigit++;
+}
+Console.WriteLine(countOfDigit);
