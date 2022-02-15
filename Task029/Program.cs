072d89dd4ad4bf699fc9b8e2c56f1b159c8bb2ba
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-15 18:52:09 +0300

    Написать программу вычисления произведения чисел от 1 до N.

---
+++ Task029/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string? num = Console.ReadLine();
+    return int.Parse(num);
+}
+
+int Multiple(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    Console.WriteLine($"Произведения всех чисел до требуемой = {result}");
+    return result;
+}
+
+int number = InPut("Введите число до которого хотите посчитать произведения чисел: ");
+Multiple(number);
