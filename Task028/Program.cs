dfbd97fc4285a32cf1ad26057b7615f7ce84eb30
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-16 22:01:31 +0300

    Подсчитать сумму цифр в числе.

---
+++ Task028/Program.cs
@@ -0,0 +1,23 @@
+﻿// Подсчитать сумму цифр в числе.
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(100, 1000000);
+    return randomNumber;
+}
+
+int SumOfDigit(int num)
+{
+    int sum = 0;
+    while (num > 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        Console.Write($"Сумма цифр в числе: {sum}");
+        return sum;
+}
+
+int num = GetNumber();
+Console.WriteLine($"Сгенерированное число: {num}");
+SumOfDigit(num);
