c289e1876d7fc6ae5b5ea963d8cbc19d2d8dc0ed
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-16 15:07:00 +0300

    Возведите число А в натуральную степень B используя цикл.

---
+++ Task026/Program.cs
@@ -0,0 +1,26 @@
+﻿// Возведите число А в натуральную степень B используя цикл.
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string? num = Console.ReadLine();
+    return int.Parse(num);
+}
+
+int APowB(int a, int b)
+{
+    int pow = 1;
+    
+    for(int i = 1; i <= b; i++)
+    {
+        pow *= a;
+    }
+    Console.WriteLine($"A в степени В = {pow}");
+    return pow;
+}
+
+int a = InPut("A = ");
+int b = InPut("B = ");
+APowB(a,b);
+
+
