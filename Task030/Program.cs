461fe9b57f7860a8b35fa8f1b8f980fbf36f47e3
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-17 12:11:17 +0300

    Показать кубы чисел, заканчивающихся на четную цифру.

---
+++ Task030/Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string? num = Console.ReadLine();
+    return int.Parse(num);
+}
+
+void CubeOfLastEvenDigit(int num)
+{ 
+int lastDigit =  num % 10;
+
+if(lastDigit % 2 == 0)
+    {
+        var cube = Math.Pow(num,3);
+        Console.Write($"Куб числа = {cube}");
+    }
+    else
+    {
+        Console.Write("Число заканчивается не на чётную цифру.");
+    }
+}       
+
+int num = InPut("Введите число: ");
+CubeOfLastEvenDigit(num);
+
