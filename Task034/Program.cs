2dd553b4fecb12fac301e40df9ae53e8b0f0d353
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-18 22:37:06 +0300

    Написать программу замену элементов массива на противоположные.

---
+++ Task034/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу замену элементов массива на противоположные.
+
+int[] arr = new int[10];
+
+void FillArray()
+{
+    Random number = new Random();
+    Console.WriteLine("Массив заполненный случайными числами: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = number.Next(1, 10);
+        Console.Write($"{arr[i]} ");
+    }
+    
+}
+
+void OppositeArray(int[] arr)
+{
+    Console.WriteLine("\nМассив с противоположными эллементами: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+       arr[i] = arr[i] * -1;
+       Console.Write($"{arr[i]} ");
+    }
+    
+}
+
+FillArray();
+OppositeArray(arr);
