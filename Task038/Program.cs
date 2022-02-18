802bba482d96552a9d85fdb7f70d157ad34c12d3
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-18 23:39:09 +0300

    Найти сумму чисел одномерного массива стоящих на нечетной позиции.

---
+++ Task038/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+
+int[] arr = new int[10];
+
+void FillArray()
+{
+    Random number = new Random();
+    Console.WriteLine("Массив заполненный случайными числами: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = number.Next(0, 10);
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+
+FillArray();
+
+int sumNeChet = 0;
+for (int i = 0; i < arr.Length; i=i+2)
+{
+    sumNeChet += arr[i];
+}
+Console.WriteLine($"\nСумма чисел стоящих на нечётной позиции = {sumNeChet}.");
