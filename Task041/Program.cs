44ea5b4b01569aa6f4d3abea229fa57e058222ed
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-20 11:29:09 +0300

    Выяснить являются ли три числа сторонами треугольника. Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других строн.

---
+++ Task041/Program.cs
@@ -0,0 +1,28 @@
+﻿// Выяснить являются ли три числа сторонами треугольника.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других строн.
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(1, 100);
+    return randomNumber;
+}
+
+bool ThreeNumSideOfTriangle(int a, int b, int c)
+{
+    return a < (b + c);
+}
+
+int numOne = GetNumber();
+int numTwo = GetNumber();
+int numThree = GetNumber();
+
+Console.WriteLine($"Первое число {numOne}, второе число {numTwo}, третье число {numThree}.");
+
+if (ThreeNumSideOfTriangle(numOne, numTwo, numThree)
+&& ThreeNumSideOfTriangle(numTwo, numOne, numThree)
+&& ThreeNumSideOfTriangle(numThree, numOne, numTwo))
+    Console.WriteLine("Числа являются сторонами сторонами треугольнтка");
+else
+    Console.WriteLine("Числа не являются сторонами сторонами треугольнтка");
+
+
