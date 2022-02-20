91cb25bdb89a067a234e71fba346a5e423036f98
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-20 13:38:49 +0300

    аписать программу преобразования десятичного числа в двоичное.

---
+++ Task043/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу преобразования десятичного числа в двоичное.
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string? num = Console.ReadLine();
+    return int.Parse(num);
+}
+int k = 1;
+int result = 0;
+int numOst = 0;
+int number = InPut("Введите число: ");
+while (number != 0)
+{
+    
+    numOst = number % 2;
+    number = number / 2;
+    result = result + numOst * k;
+    k *=10;
+}
+Console.WriteLine($"В десятичном виде = {result}");
