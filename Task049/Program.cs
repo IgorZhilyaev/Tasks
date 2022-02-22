f377c68d14a770463217891e189a3733a60c3cfb
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-22 19:51:42 +0300

    Показать двумерный массив размером mxn заполненный вещественными числами.

---
+++ Task049/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами.
+
+double[,] array = new double[6, 5];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = Math.Round(rand.NextDouble() * 100, 2);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray();
+PrintArray();
