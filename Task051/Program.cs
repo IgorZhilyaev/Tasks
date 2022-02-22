84dc817da8ab2551c256a5db2c1a03951fea748c
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-22 20:27:12 +0300

    Задать двумерный массив следующим правилом: Aₘₙ = m+n.

---
+++ Task051/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+Console.Clear();
+
+int[,] array = new int[3, 3];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MethodSum() 
+{
+    int[,] arr = new int[array.GetLength(0),array.GetLength(1)];   
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+
+           arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+FillArray();
+PrintArray(array);
+Console.WriteLine("----------------");
+int[,] arr = MethodSum();
+PrintArray(arr);
