5dfb2e0baf71f72638078ca2900fa18bff199f57
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-24 19:36:23 +0300

    Показать двумерный массив размером m×n заполненный целыми числами.

---
+++ Task048/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами.
+
+double[,] array = new double[6, 5];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rand.Next(1,10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray();
+PrintArray();
