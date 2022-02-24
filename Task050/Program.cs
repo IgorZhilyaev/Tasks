68c76bb72fc5ed6473ec288f8a427c89c4cdec68
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-24 20:03:54 +0300

    В двумерном массиве n×k заменить четные элементы на противоположные.

---
+++ Task050/Program.cs
@@ -0,0 +1,49 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные.
+
+int[,] array = new int[6, 5];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rand.Next(1,10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceElements()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0)
+            {
+                array[i, j] *= -1; 
+            }
+            Console.Write($"{array[i, j]}   ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
+Console.WriteLine();
+ReplaceElements();
