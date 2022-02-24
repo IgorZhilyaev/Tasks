f29d024dbcf7259237b4eb7f6d653649eb9a6966
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-24 21:21:06 +0300

    В матрице чисел найти сумму элементов главной диагонали.

---
+++ Task054/Program.cs
@@ -0,0 +1,48 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали.
+
+int[,] array = new int[5, 5];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumDiagonal()
+{
+    int sumDiagonal = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            
+            if (i == j)
+            {
+                sumDiagonal += array[i, j];              
+            }          
+        }
+    }
+Console.Write($"Сумма элементов главной диагонали = {sumDiagonal}");
+}
+
+FillArray();
+PrintArray();
+SumDiagonal();
