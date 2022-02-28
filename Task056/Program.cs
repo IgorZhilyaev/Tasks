c1f1c1e0ec5471f03face9466ae3d287a47c060d
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-02-28 12:23:26 +0300

    Написать программу, которая обменивает элементы первой строки и последней строки.

---
+++ Task056/Program.cs
@@ -0,0 +1,45 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки.
+
+int[,] array = new int[5, 5];
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapArray()
+{
+    int lastStr = array.GetLength(0) - 1;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int saveNum = array[0,i];
+        array[0,i] = array[lastStr,i];
+        array[lastStr, i] = saveNum;
+    }
+}
+
+FillArray();
+PrintArray();
+SwapArray();
+Console.WriteLine();
+PrintArray();
