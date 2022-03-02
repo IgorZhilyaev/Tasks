a08c58cb628e7d007d3e8c3aba1a872bcb7a7f88
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-03-02 12:42:52 +0300

    Составить частотный словарь элементов двумерного массива.

---
+++ Task60/Program.cs
@@ -0,0 +1,26 @@
+﻿// Составить частотный словарь элементов двумерного массива.
+
+int[] array = new int[] { 9, 8, 4, 4, 5, 6, 1, 2, 47, 8, 5, 9, 6, 4 };
+
+Dictionary<int, int> Execute()
+{
+    Dictionary<int, int> dic = new Dictionary<int, int>();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (dic.ContainsKey(array[i]))
+            dic[array[i]] = dic[array[i]] + 1;
+        else
+            dic.Add(array[i], 1);
+    }
+    return dic;
+}
+
+void PrintDic(Dictionary<int, int> dic)
+{
+    foreach (var item in dic)
+    {
+        Console.WriteLine($"{item.Key} встречается {item.Value} раз.");
+    }
+}
+
+PrintDic(Execute());
