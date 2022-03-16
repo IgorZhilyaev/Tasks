65e828f85cdf677d86c0e15a732f42bb4740b68f
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-03-16 20:44:10 +0300

    Спирально заполнить двумерный массив.

---
+++ Task065/Program.cs
@@ -0,0 +1,39 @@
+﻿// Спирально заполнить двумерный массив.
+
+         
+int[,] array = new int[3, 3];
+ 
+void SpiralArray(int[,] array)
+{   
+    int row = 0, col = 0, dx = 0, dy = 1, dirChanges = 0, gran = array.GetLength(0);
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[col, row] = i + 1;
+        if (--gran == 0)
+        {
+            gran = array.GetLength(0) * (dirChanges % 2) + array.GetLength(1) * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+            int temp = dy;
+            dy = -dx;
+            dx = temp;
+            dirChanges++;
+            
+        }
+        col += dx;
+        row += dy;
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+SpiralArray(array);
+PrintArray(array);
